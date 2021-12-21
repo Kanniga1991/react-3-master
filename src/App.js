751434168c79099a460a269c599d75df87a7c5f1
Kanniga1991 <deeprakaash@gmail.com>
2021-12-21 12:18:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Card from './Card';
+import Topbar from './Topbar';
+import CartList from './CartList';
+import { BrowserRouter,Switch,Route } from "react-router-dom";
+import { DataProvider } from './DataContext';
+import React from 'react';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <DataProvider>
+          <Topbar></Topbar>
+          <Route path="/" component={Card} exact={true}></Route>
+          <Route path="/cart" component={CartList} exact={true}></Route>
+        </DataProvider>
+      </Switch>
+    </BrowserRouter>
+  );
+}
